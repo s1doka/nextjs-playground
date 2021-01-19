3cc976efb0d8cddd36efcfd34a1477052a694dc7
Pedro Henrique <ph.barbosilva@gmail.com>
2021-01-18 22:22:03 -0300

    fix: added missing Head component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Document, {Html, Main, NextScript} from "next/document";
+import Document, {Html, Main, NextScript, Head} from "next/document";
 import {ServerStyleSheets} from "@material-ui/core/styles";
 
 export default class _document extends Document {
 	render() {
 		return <Html lang="en">
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
