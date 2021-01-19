77005d0d485849ca2d47c172db4a693f9a511391
Pedro Henrique <ph.barbosilva@gmail.com>
2021-01-18 22:16:12 -0300

    refactor: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,48 +1,42 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
 import {useStyles} from "./styles";
 
-const Header = () => {
+function Header() {
 	const classes = useStyles();
 
-	return (
-		<div className={classes.root}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						edge="start"
-						className={classes.menuButton}
-						color="inherit"
-						aria-label="open drawer"
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography className={classes.title} variant="h6" noWrap>
-						Candy Cinema
-					</Typography>
-					<div className={classes.search}>
-						<div className={classes.searchIcon}>
-							<SearchIcon />
-						</div>
-						<InputBase
-							placeholder="Search…"
-							classes={{
-								root: classes.inputRoot,
-								input: classes.inputInput,
-							}}
-							inputProps={{ 'aria-label': 'search' }}
-						/>
+	return <div className={classes.root}>
+		<AppBar position="static">
+			<Toolbar>
+				<IconButton edge="start"
+				className={classes.menuButton}
+				color="inherit"
+				aria-label="open drawer">
+					<MenuIcon />
+				</IconButton>
+				<Typography className={classes.title} variant="h6" noWrap={true}>
+					Candy Cinema
+				</Typography>
+				<div className={classes.search}>
+					<div className={classes.searchIcon}>
+						<SearchIcon />
 					</div>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+					<InputBase placeholder="Search&hellip;"
+					classes={{
+						root: classes.inputRoot,
+						input: classes.inputInput,
+					}}
+					inputProps={{"aria-label": "search"}} />
+				</div>
+			</Toolbar>
+		</AppBar>
+	</div>;
 }
 
-export default Header
+export default Header;
